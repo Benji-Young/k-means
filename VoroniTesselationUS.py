57b92ba532a8305186915311ca052ead7deb301e
Benji-Young <benjaminty2003@gmail.com>
2024-03-18 23:16:53 +0000

    Added VoroniTesselation

--- VoroniTesselationUS.py
+++ VoroniTesselationUS.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d
 from matplotlib import pyplot as plt
 import pandas as pd
+import helper
 
 state_data = pd.read_csv("states.csv")
 coordinates = state_data[["longitude", "latitude"]].to_numpy()
@@ -14,16 +15,16 @@ for i in range(len(coordinates)):
 #plt.scatter(coordinates[:,0], coordinates[:,1])
 
 vor = Voronoi(coordinates)
+# Get the finite regions
+regions, vertices = helper.voronoi_finite_polygons_2d(vor)
 
-# Plot the Voronoi diagram with custom colors
-fig = voronoi_plot_2d(vor, show_vertices = False)
-
-# colorize
-for region in vor.regions:
-    polygon = vor.vertices[region]
-    plt.fill(*zip(*polygon), alpha=0.4)
+# Colourize the regions
+for region in regions:
+    polygon = vertices[region]
+    plt.fill(*zip(*polygon), alpha=0.9)
 
 plt.plot(coordinates[:,0], coordinates[:,1], 'ko')
-
+plt.xlim(min(coordinates[:,0])-10, max(coordinates[:,0])+10)
+plt.ylim(min(coordinates[:,1])-10, max(coordinates[:,1])+10)
 plt.show()
 
